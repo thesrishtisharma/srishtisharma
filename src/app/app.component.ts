93e946fa890790e83eaa07fe5208ff8c8f3bd966
Srishti Sharma <38985008+thesrishtisharma@users.noreply.github.com>
2025-03-22 01:10:26 +0530

    Revert "20032024 | Removing themes"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -62,7 +62,7 @@ export class AppComponent implements AfterViewInit, OnInit{
   logo_image: string = "NoThemeLogo.png";
   navNameColor:string = '#F59E0B';
   headings: string = '#F59E0B';
-  checked = false;
+  checked = true;
   disabled = false;
   
   experience_data = [
